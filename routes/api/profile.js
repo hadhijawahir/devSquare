f96880ee718da11f8130e199edde09c5ff89387d
hadhijawahir <hadhi@live.com>
2019-09-14 00:26:20 +0530

    -

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -6,6 +6,7 @@ const passport = require('passport');
 
 // Load Validation
 const validateProfileInput = require('../../validation/profile');
+const validateExperienceInput = require('../../validation/experience');
 
 //  Load Profile Models
 const Profile = require('../../models/Profile');
@@ -173,5 +174,35 @@ router.post(
     }
   );
 
+// @route   POST api/profile/experience
+// @desc    Add experience to profile
+// @access  Private
+router.post('/experience', passport.authenticate('jwt', { session: false }), (req,res) => {
+    const { errors, isValid } = validateExperienceInput(req.body);
+  
+    // Check Validation
+    if (!isValid) {
+        // Return any errors with 400 status
+        return res.status(400).json(errors);
+    }
+
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            const newExp = {
+                title: req.body.title,
+                company: req.body.company,
+                location: req.body.title,
+                from: req.body.from,
+                to: req.body.to,
+                current: req.body.current,
+                description: req.body.description
+            }
+
+            // Add to experience array
+            profile.experience.unshift(newExp);
+
+            profile.save().then(profile => res.json(profile));
+        })
+});
 
 module.exports = router;
