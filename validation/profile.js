f68ba10028a1b8f06c901841a0cd85833867a4c4
hadhijawahir <hadhi@live.com>
2019-09-13 22:00:51 +0530

    -

--- validation/profile.js
+++ validation/profile.js
@@ -12,16 +12,52 @@ module.exports = function validateProfileInput(data) {
     errors.handle = 'Handle needs to be between 2 and 40 characters';
   }
 
-  if (!Validator.isEmpty(data.handle)) {
+  if (Validator.isEmpty(data.handle)) {
     errors.handle = 'Profile Handle is required';
   }
 
-  if (!Validator.isEmpty(data.status)) {
+  if (Validator.isEmpty(data.status)) {
     errors.status = 'Status Field is required';
   }
 
-  if (!Validator.isEmpty(data.skills)) {
+  if (Validator.isEmpty(data.skills)) {
     errors.skills = 'Skills Field is required';
+  }
+
+  if (!isEmpty(data.website)){
+    if (!Validator.isURL(data.website)) {
+      errors.website = 'Not a valid URL';
+    }
+  }
+
+  if (!isEmpty(data.youtube)){
+    if (!Validator.isURL(data.youtube)) {
+      errors.youtube = 'Not a valid URL';
+    }
+  }
+
+  if (!isEmpty(data.twitter)){
+    if (!Validator.isURL(data.twitter)) {
+      errors.twitter = 'Not a valid URL';
+    }
+  }
+
+  if (!isEmpty(data.facebook)){
+    if (!Validator.isURL(data.facebook)) {
+      errors.facebook = 'Not a valid URL';
+    }
+  }
+
+  if (!isEmpty(data.linkedin)){
+    if (!Validator.isURL(data.linkedin)) {
+      errors.linkedin = 'Not a valid URL';
+    }
+  }
+  
+  if (!isEmpty(data.instagram)){
+    if (!Validator.isURL(data.instagram)) {
+      errors.instagram = 'Not a valid URL';
+    }
   }
   
   return {
